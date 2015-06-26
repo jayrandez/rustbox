b3f6f647a59a570c231809d3e3e8cd714e7c6111
Jay Randez <jayrandez@gmail.com>
2015-06-26 18:30:58 -0400

    Copying public API from Linux version.

--- src/rustbox-pure/mod.rs
+++ src/rustbox-pure/mod.rs
@@ -1,2 +1,358 @@
 extern crate winapi;
 extern crate kernel32;
+
+pub mod keyboard;
+pub mod mouse;
+
+pub use self::keyboard::Key;
+pub use self::mouse::Mouse;
+pub use self::running::running;
+pub use self::style::{Style, RB_BOLD, RB_UNDERLINE, RB_REVERSE, RB_NORMAL};
+
+use std::default::Default;
+use std::error::Error;
+use std::{fmt, io, char};
+use num::FromPrimitive;
+use libc::c_int;
+use time::Duration;
+
+#[derive(Clone, Copy)]
+pub enum Event {
+    KeyEventRaw(u8, u16, u32),
+    KeyEvent(Option<Key>),
+    ResizeEvent(i32, i32),
+    MouseEvent(Mouse, i32, i32),
+    NoEvent
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum InputMode {
+    Current = 0x00,
+
+    /// When ESC sequence is in the buffer and it doesn't match any known
+    /// ESC sequence => ESC means TB_KEY_ESC
+    Esc     = 0x01,
+    /// When ESC sequence is in the buffer and it doesn't match any known
+    /// sequence => ESC enables TB_MOD_ALT modifier for the next keyboard event.
+    Alt     = 0x02,
+    /// Same as `Esc` but enables mouse events
+    EscMouse = 0x05,
+    /// Same as `Alt` but enables mouse events
+    AltMouse = 0x06
+}
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(C,u16)]
+pub enum Color {
+    Default =  0x00,
+    Black =    0x01,
+    Red =      0x02,
+    Green =    0x03,
+    Yellow =   0x04,
+    Blue =     0x05,
+    Magenta =  0x06,
+    Cyan =     0x07,
+    White =    0x08,
+}
+
+mod style {
+    bitflags! {
+        #[repr(C)]
+        flags Style: u16 {
+            const TB_NORMAL_COLOR = 0x000F,
+            const RB_BOLD = 0x0100,
+            const RB_UNDERLINE = 0x0200,
+            const RB_REVERSE = 0x0400,
+            const RB_NORMAL = 0x0000,
+            const TB_ATTRIB = RB_BOLD.bits | RB_UNDERLINE.bits | RB_REVERSE.bits,
+        }
+    }
+
+    impl Style {
+        pub fn from_color(color: super::Color) -> Style {
+            Style { bits: color as u16 & TB_NORMAL_COLOR.bits }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum EventError {
+   TermboxError,
+   Unknown(isize),
+}
+
+impl fmt::Display for EventError {
+   fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+      write!(fmt, "{}", self.description())
+   }
+}
+
+impl Error for EventError {
+   fn description(&self) -> &str {
+      match *self {
+         EventError::TermboxError => "Error in Termbox",
+         // I don't know how to format this without lifetime error.
+         // EventError::Unknown(n) => &format!("There was an unknown error. Error code: {}", n),
+         EventError::Unknown(_) => "Unknown error in Termbox",
+      }
+   }
+}
+
+impl FromPrimitive for EventError {
+   fn from_i64(n: i64) -> Option<EventError> {
+      match n {
+         -1 => Some(EventError::TermboxError),
+         n => Some(EventError::Unknown(n as isize)),
+      }
+   }
+
+   fn from_u64(n: u64) -> Option<EventError> {
+      Some(EventError::Unknown(n as isize))
+   }
+}
+
+pub type EventResult = Result<Event, EventError>;
+
+/// Unpack a RawEvent to an Event
+///
+/// if the `raw` parameter is true, then the Event variant will be the raw
+/// representation of the event.
+///     for instance KeyEventRaw instead of KeyEvent
+///
+/// This is useful if you want to interpret the raw event data yourself, rather
+/// than having rustbox translate it to its own representation.
+fn unpack_event(ev_type: c_int, /*ev: &RawEvent,*/ raw: bool) -> EventResult {
+    Ok(Event::NoEvent)
+}
+
+#[derive(Debug)]
+pub enum InitError {
+    BufferStderrFailed(io::Error),
+    AlreadyOpen,
+    UnsupportedTerminal,
+    FailedToOpenTTy,
+    PipeTrapError,
+    Unknown(isize),
+}
+
+impl fmt::Display for InitError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}", self.description())
+    }
+}
+
+impl Error for InitError {
+    fn description(&self) -> &str {
+        match *self {
+            InitError::BufferStderrFailed(_) => "Could not redirect stderr",
+            InitError::AlreadyOpen => "RustBox is already open",
+            InitError::UnsupportedTerminal => "Unsupported terminal",
+            InitError::FailedToOpenTTy => "Failed to open TTY",
+            InitError::PipeTrapError => "Pipe trap error",
+            InitError::Unknown(_) => "Unknown error from Termbox",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            InitError::BufferStderrFailed(ref e) => Some(e),
+            _ => None
+        }
+    }
+}
+
+impl FromPrimitive for InitError {
+   fn from_i64(n: i64) -> Option<InitError> {
+      match n {
+         -1 => Some(InitError::UnsupportedTerminal),
+         -2 => Some(InitError::FailedToOpenTTy),
+         -3 => Some(InitError::PipeTrapError),
+         n => Some(InitError::Unknown(n as isize)),
+      }
+   }
+
+   fn from_u64(n: u64) -> Option<InitError> {
+      Some(InitError::Unknown(n as isize))
+   }
+}
+
+#[allow(missing_copy_implementations)]
+pub struct RustBox {
+    // We only bother to redirect stderr for the moment, since it's used for panic!
+    //_stderr: Option<Hold>,
+    // RAII lock.
+    //
+    // Note that running *MUST* be the last field in the destructor, since destructors run in
+    // top-down order. Otherwise it will not properly protect the above fields.
+    _running: running::RunningGuard,
+}
+
+// Termbox is not thread safe
+impl !Send for RustBox {}
+
+#[derive(Clone, Copy,Debug)]
+pub struct InitOptions {
+    /// Use this option to initialize with a specific input mode
+    ///
+    /// See InputMode enum for details on the variants.
+    pub input_mode: InputMode,
+
+    /// Use this option to automatically buffer stderr while RustBox is running.  It will be
+    /// written when RustBox exits.
+    ///
+    /// This option uses a nonblocking OS pipe to buffer stderr output.  This means that if the
+    /// pipe fills up, subsequent writes will fail until RustBox exits.  If this is a concern for
+    /// your program, don't use RustBox's default pipe-based redirection; instead, redirect stderr
+    /// to a log file or another process that is capable of handling it better.
+    pub buffer_stderr: bool,
+}
+
+impl Default for InitOptions {
+    fn default() -> Self {
+        InitOptions {
+            input_mode: InputMode::Current,
+            buffer_stderr: false,
+        }
+    }
+}
+
+mod running {
+    use std::sync::atomic::{self, AtomicBool};
+
+    // The state of the RustBox is protected by the lock. Yay, global state!
+    static RUSTBOX_RUNNING: AtomicBool = atomic::ATOMIC_BOOL_INIT;
+
+    /// true iff RustBox is currently running. Beware of races here--don't rely on this for anything
+    /// critical unless you happen to know that RustBox cannot change state when it is called (a good
+    /// usecase would be checking to see if it's worth risking double printing backtraces to avoid
+    /// having them swallowed up by RustBox).
+    pub fn running() -> bool {
+        RUSTBOX_RUNNING.load(atomic::Ordering::SeqCst)
+    }
+
+    // Internal RAII guard used to ensure we release the running lock whenever we acquire it.
+    #[allow(missing_copy_implementations)]
+    pub struct RunningGuard(());
+
+    pub fn run() -> Option<RunningGuard> {
+        // Ensure that we are not already running and simultaneously set RUSTBOX_RUNNING using an
+        // atomic swap. This ensures that contending threads don't trample each other.
+        if RUSTBOX_RUNNING.swap(true, atomic::Ordering::SeqCst) {
+            // The Rustbox was already running.
+            None
+        } else {
+            // The RustBox was not already running, and now we have the lock.
+            Some(RunningGuard(()))
+        }
+    }
+
+    impl Drop for RunningGuard {
+        fn drop(&mut self) {
+            // Indicate that we're free now. We could probably get away with lower atomicity here,
+            // but there's no reason to take that chance.
+            RUSTBOX_RUNNING.store(false, atomic::Ordering::SeqCst);
+        }
+    }
+}
+
+impl RustBox {
+    /// Initialize rustbox.
+    ///
+    /// For the default options, you can use:
+    ///
+    /// ```
+    /// use rustbox::RustBox;
+    /// use std::default::Default;
+    /// let rb = RustBox::init(Default::default());
+    /// ```
+    ///
+    /// Otherwise, you can specify:
+    ///
+    /// ```
+    /// use rustbox::{RustBox, InitOptions};
+    /// use std::default::Default;
+    /// let rb = RustBox::init(InitOptions { input_mode: rustbox::InputMode::Esc, ..Default::default() });
+    /// ```
+    pub fn init(opts: InitOptions) -> Result<RustBox, InitError> {
+        let running = match running::run() {
+            Some(r) => r,
+            None => return Err(InitError::AlreadyOpen),
+        };
+
+        // Create the RustBox.
+        let rb = RustBox {
+            //_stderr: None,
+            _running: running
+        };
+
+        match opts.input_mode {
+            InputMode::Current => (),
+            _ => rb.set_input_mode(opts.input_mode),
+        }
+
+        Ok(rb)
+    }
+
+    pub fn width(&self) -> usize {
+        0
+    }
+
+    pub fn height(&self) -> usize {
+        0
+    }
+
+    pub fn clear(&self) {
+
+    }
+
+    pub fn present(&self) {
+
+    }
+
+    pub fn set_cursor(&self, x: isize, y: isize) {
+
+    }
+
+    pub unsafe fn change_cell(&self, x: usize, y: usize, ch: u32, fg: u16, bg: u16) {
+
+    }
+
+    pub fn print(&self, x: usize, y: usize, sty: Style, fg: Color, bg: Color, s: &str) {
+        let fg = Style::from_color(fg) | (sty & style::TB_ATTRIB);
+        let bg = Style::from_color(bg);
+        for (i, ch) in s.chars().enumerate() {
+            unsafe {
+                self.change_cell(x+i, y, ch as u32, fg.bits(), bg.bits());
+            }
+        }
+    }
+
+    pub fn print_char(&self, x: usize, y: usize, sty: Style, fg: Color, bg: Color, ch: char) {
+        let fg = Style::from_color(fg) | (sty & style::TB_ATTRIB);
+        let bg = Style::from_color(bg);
+        unsafe {
+            self.change_cell(x, y, ch as u32, fg.bits(), bg.bits());
+        }
+    }
+
+    pub fn poll_event(&self, raw: bool) -> EventResult {
+        Ok(Event::NoEvent)
+    }
+
+    pub fn peek_event(&self, timeout: Duration, raw: bool) -> EventResult {
+        Ok(Event::NoEvent)
+    }
+
+    pub fn set_input_mode(&self, mode: InputMode) {
+
+    }
+}
+
+impl Drop for RustBox {
+    fn drop(&mut self) {
+        // Since only one instance of the RustBox is ever accessible, we should not
+        // need to do this atomically.
+        // Note: we should definitely have RUSTBOX_RUNNING = true here.
+
+    }
+}
